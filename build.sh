4da2f87442240ddb3be18b1393b6ac30bf4bfef9
Zhicheng Zhang <zhichengzhang00@umail.ucsb.edu>
2019-08-15 11:34:49 +0800

    t

---
+++ build.sh
@@ -0,0 +1,13 @@
+sudo git clone https://github.com/F5Networks/f5-adcaas-openstack.git
+sudo apt-get update
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent     software-properties-common -y
+sudo apt install docker.io -y
+sudo npm install -g typescript -y
+sudo apt install python-pip -y
+sudo pip install docker-compose
+
+# waf:3
+# script:1
+# test:1
