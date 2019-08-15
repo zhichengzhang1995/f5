2e5089060a394bcf427d55faddccce50e5597c08
Zhicheng Zhang <zhichengzhang00@umail.ucsb.edu>
2019-08-15 12:10:20 +0800

    t

--- build-1.sh
+++ build-1.sh
@@ -6,7 +6,9 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent     so
 sudo apt install docker.io -y
 sudo npm install -g typescript -y
 sudo apt install python-pip -y
-sudo pip install docker-compose
+# sudo pip install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # waf:3
 # script:1
